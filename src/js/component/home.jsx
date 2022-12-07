652860da36ad900072f68f8db0ea58ffff898b0d
MATIAS EXEQUIEL CECCI <matias_cecci@hotmail.com>
2022-12-07 16:50:02 +0000

    Agregado de prop al jumbotron

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,7 +9,17 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-
+		<div className="fluid">
+			<Navbar/>
+			<div className="container">
+				
+				<Jumbotron
+				titleJumbotron= "A warm welcome!"
+				textJumbotron= "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat quam delectus asperiores nemo adipisci ea dolorum amet est, doloribus repudiandae fuga vero aliquam excepturi nam qui perspiciatis sequi voluptas obcaecati?"
+				buttonJumbotron= "Call to action!"
+				/>
+			</div>
+		</div>
 	);
 };
 
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,23 +5,28 @@ import React from "react";
 //create your first component
 const Navbar = () => {
 	return (
-		<nav class="navbar bg-dark navbar-expand-lg">
-			<div class="container-fluid">
-				<a class="navbar-brand text-white">Start Bootstrap</a>
-				<ul class="navbar-nav ">
-					<li class="nav-item">
-						<a class="nav-link active text-white" aria-current="page" href="#">Home</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link text-light" href="#">About</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link text-light" href="#">Services</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link text-light" href="#">Contact</a>
-					</li>	
-				</ul>
+		<nav class="navbar navbar-dark bg-dark navbar-expand-md ">
+			<div class="container-md">
+				<a class="navbar-brand text-white fs-3">Start Bootstrap</a>
+				<button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon  "></span>
+				</button>
+				<div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-secondary" href="#">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-secondary" href="#">Services</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-secondary" href="#">Contact</a>
+						</li>	
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);

fbf26192da60269deb88d5bd0828a3f6c3d150b5
Spyravis <ritalopezcobo@gmail.com>
2022-12-07 14:51:10 +0000

    body

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import Card from "./card";
 import Navbar from "./navbar";
-
 import Jumbotron from "./jumbotron";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
 
-	);
+  return (
+    <>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 justify-content-center">
+          <Card/>
+		  <Card/>
+          <Card/>
+          <Card/>
+        </div>
+      </div>
+    </>
+  );
 };
-
 export default Home;
